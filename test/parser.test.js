00fa8c3543918aad6be225c19d97092a069644b8
TANNER MECK <steve.meck246@gmail.com>
2021-09-20 18:28:45 -0700

    one test passing for body parser, second test not passing due to reject statement

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,27 +1,26 @@
 import request from 'supertest';
-import { parser } from '../lib/parser.js';
+import { parser } from '../lib/app.js';
 
 describe('takes a http request object and returns a parsed body', () => {
   it('should return null if a method is not POST, PUT, or PATCH', async () => {
-    const res = await request(parser).get('/');
+    const value = await parser({ method: 'GET' });
 
-    expect(res.status).toEqual(404);
+    expect(value).toEqual(null);
   });
   it('throws error if content-type is not application/json', async () => {
-    const newObject = { body: 'hello-world', status: 200, contentType: 'text/plain' };
-    const res = await request(parser).send(newObject);
+    const newObject = { method: 'POST', body: 'hello-world', status: 200, contentType: 'text/plain' };
+    const res = await parser(newObject);
 
     expect(res.status).toEqual(404);
   });
-  it('returns a deserialized body from req emitted events', async () => {
+  xit('returns a deserialized body from req emitted events', async () => {
     const res = await request(parser);
 
     expect(res.body).toEqual();
   });
-  it('throws an error if failure happens in deserialization', async () => {
+  xit('throws an error if failure happens in deserialization', async () => {
     const res = await request(parser);
 
     expect(res.status).toEqual(404);
-
   });
 });
